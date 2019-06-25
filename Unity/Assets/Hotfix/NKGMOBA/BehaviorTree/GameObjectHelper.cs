45530107e9b5ef904c74868f0df85396c1b81f43
NKGMadlife <1778139321@qq.com>
2019-06-26 07:20:12 +0800

    客户端整合可热更行为树，为技能系统表现层做准备

---
+++ Unity/Assets/Hotfix/NKGMOBA/BehaviorTree/GameObjectHelper.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace ETHotfix
+{
+    public static class GameObjectHelper
+    {
+        public static T Ensure<T>(this GameObject gameObject) where T : UnityEngine.Component
+        {
+            if (gameObject)
+            {
+                var t = gameObject.GetComponent<T>();
+
+                if (!t)
+                {
+                    t = gameObject.AddComponent<T>();
+                }
+
+                return t;
+            }
+
+            return null;
+        }
+
+        public static T Ensure<T>(this UnityEngine.Component component) where T : UnityEngine.Component
+        {
+            if (component)
+            {
+                return Ensure<T>(component.gameObject);
+            }
+
+            return null;
+        }
+    }
+}
