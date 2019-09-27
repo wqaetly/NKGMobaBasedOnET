8226375f8045124e238a9b6c3188a1fe2993c4b5
NKGMadlife <1778139321@qq.com>
2019-09-27 22:29:37 +0800

    技能可视化编辑器：增加生成碰撞体结点，遇到了问题，因为这块逻辑在服务端，在想解决方案

---
+++ Unity/Assets/Model/NKGMOBA/NPBehave/NodeDatas/TheDataContainsAction/NP_CreateColliderDataAction.cs
@@ -0,0 +1,32 @@
+//------------------------------------------------------------
+// Author: 烟雨迷离半世殇
+// Mail: 1778139321@qq.com
+// Data: 2019年9月27日 22:26:39
+//------------------------------------------------------------
+
+using System;
+using ETModel.TheDataContainsAction;
+using Sirenix.OdinInspector;
+
+namespace Model.NKGMOBA.NPBehave.NodeDatas.TheDataContainsAction
+{
+    public class NP_CreateColliderData:NP_ClassForStoreAction
+    {
+        [LabelText("要生成的碰撞数据载体ID")]
+        public long supportDataID;
+        
+        [LabelText("要生成的碰撞数据ID")]
+        public long colliderDataID;
+        
+        public override Action GetActionToBeDone()
+        {
+            this.m_Action = this.CreateColliderData;
+            return this.m_Action;
+        }
+
+        public void CreateColliderData()
+        {
+            
+        }
+    }
+}
