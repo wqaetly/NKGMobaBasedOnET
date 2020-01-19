248e3dfbae79ff113d9f435e45ea21b94a7f4709
NKGMadlife <1778139321@qq.com>
2020-01-19 15:47:41 +0800

    客户端通过AB包读取技能配置

--- Unity/Assets/Model/NKGMOBA/NPBehave/Component/NP_TreeRepository.cs
+++ Unity/Assets/Model/NKGMOBA/NPBehave/Component/NP_TreeRepository.cs
@@ -53,6 +53,7 @@ namespace ETModel
                 BsonClassMap.LookupClassMap(type);
             }
 
+#if SERVER
             DirectoryInfo directory = new DirectoryInfo(NPDataPath);
             FileInfo[] fileInfos = directory.GetFiles();
 
@@ -76,6 +77,60 @@ namespace ETModel
                     throw;
                 }
             }
+#else
+            if (!Define.ResModeIsEditor)
+            {
+                ResourcesComponent resourcesComponent = Game.Scene.GetComponent<ResourcesComponent>();
+                resourcesComponent.LoadBundle("skillconfigs.unity3d");
+                GameObject skillConfigs = (GameObject) resourcesComponent.GetAsset("skillconfigs.unity3d", "SkillConfigs");
+                foreach (var VARIABLE in skillConfigs.GetComponent<ReferenceCollector>().data)
+                {
+                    TextAsset textAsset = skillConfigs.Get<TextAsset>(VARIABLE.key);
+                    
+                    if (textAsset.bytes.Length == 0) Log.Info("没有读取到文件");
+
+                    try
+                    {
+                        NP_DataSupportor MnNpDataSupportor = BsonSerializer.Deserialize<NP_DataSupportor>(textAsset.bytes);
+
+                        Log.Info($"反序列化行为树:{VARIABLE.key}完成");
+
+                        NpRuntimeTreesDatas.Add(MnNpDataSupportor.RootId, MnNpDataSupportor);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        throw;
+                    }
+                }
+            }
+            else
+            {
+                DirectoryInfo directory = new DirectoryInfo(NPDataPath);
+                FileInfo[] fileInfos = directory.GetFiles();
+
+                foreach (var VARIABLE in fileInfos)
+                {
+                    byte[] mfile = File.ReadAllBytes(VARIABLE.FullName);
+
+                    if (mfile.Length == 0) Log.Info("没有读取到文件");
+
+                    try
+                    {
+                        NP_DataSupportor MnNpDataSupportor = BsonSerializer.Deserialize<NP_DataSupportor>(mfile);
+
+                        Log.Info($"反序列化行为树:{VARIABLE.FullName}完成");
+
+                        NpRuntimeTreesDatas.Add(MnNpDataSupportor.RootId, MnNpDataSupportor);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        throw;
+                    }
+                }
+            }
+#endif
         }
 
         /// <summary>
