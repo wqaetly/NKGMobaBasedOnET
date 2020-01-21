8c5d2b018d81611927f3a390e43941d334d6b743
NKGMadlife <1778139321@qq.com>
2020-01-21 21:16:01 +0800

    增加子实体组件，用于实现诸如召唤物，野怪，这种不需要额外做session同步的实体

---
+++ Unity/Assets/Model/NKGMOBA/Component/ChildrenUnitComponent.cs
@@ -0,0 +1,38 @@
+//------------------------------------------------------------
+// Author: 烟雨迷离半世殇
+// Mail: 1778139321@qq.com
+// Data: 2020年1月21日 12:18:03
+//------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace ETModel
+{
+    /// <summary>
+    /// 子实体组件，用于管理unit产生的子实体
+    /// 一般用于，召唤物，有关联的实体
+    /// 以及那些可能并不需要网络通讯的子实体，训练营的那个木桩就是最好的例子
+    /// </summary>
+    public class ChildrenUnitComponent: Component
+    {
+        /// <summary>
+        /// 用于管理子实体
+        /// </summary>
+        public List<Unit> ChildrenUnit = new List<Unit>();
+
+        public void AddUnit(Unit unit)
+        {
+            this.ChildrenUnit.Add(unit);
+        }
+
+        public void RemoveUnit(Unit unit)
+        {
+            this.ChildrenUnit.Remove(unit);
+        }
+
+        public Unit GetUnit(long id)
+        {
+            return this.ChildrenUnit.Find(s => s.Id.Equals(id));
+        }
+    }
+}
