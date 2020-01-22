830947dd2dcbcd3885fd7eb18558a433236a8fce
NKGMadlife <1778139321@qq.com>
2020-01-22 09:39:08 +0800

    Warning！！！：更改了FGUI进度条Tween模块源码

--- Unity/Assets/ThirdParty/FairyGUI/Runtime/Scripts/UI/GProgressBar.cs
+++ Unity/Assets/ThirdParty/FairyGUI/Runtime/Scripts/UI/GProgressBar.cs
@@ -4,141 +4,151 @@ using FairyGUI.Utils;
 
 namespace FairyGUI
 {
-	/// <summary>
-	/// GProgressBar class.
-	/// </summary>
-	public class GProgressBar : GComponent
-	{
-		double _max;
-		double _value;
-		ProgressTitleType _titleType;
-		bool _reverse;
+    /// <summary>
+    /// GProgressBar class.
+    /// </summary>
+    public class GProgressBar: GComponent
+    {
+        double _max;
+        double _value;
+        ProgressTitleType _titleType;
+        bool _reverse;
 
-		GTextField _titleObject;
-		GMovieClip _aniObject;
-		GObject _barObjectH;
-		GObject _barObjectV;
-		float _barMaxWidth;
-		float _barMaxHeight;
-		float _barMaxWidthDelta;
-		float _barMaxHeightDelta;
-		float _barStartX;
-		float _barStartY;
-		bool _tweening;
+        GTextField _titleObject;
+        GMovieClip _aniObject;
+        GObject _barObjectH;
+        GObject _barObjectV;
+        float _barMaxWidth;
+        float _barMaxHeight;
+        float _barMaxWidthDelta;
+        float _barMaxHeightDelta;
+        float _barStartX;
+        float _barStartY;
+        bool _tweening;
 
-		public GProgressBar()
-		{
-			_value = 50;
-			_max = 100;
-		}
+        public GProgressBar()
+        {
+            _value = 50;
+            _max = 100;
+        }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		public ProgressTitleType titleType
-		{
+        /// <summary>
+        /// 
+        /// </summary>
+        public ProgressTitleType titleType
+        {
+            get
+            {
+                return _titleType;
+            }
+            set
+            {
+                if (_titleType != value)
+                {
+                    _titleType = value;
+                    Update(_value);
+                }
+            }
+        }
 
-			get
-			{
-				return _titleType;
-			}
-			set
-			{
-				if (_titleType != value)
-				{
-					_titleType = value;
-					Update(_value);
-				}
-			}
-		}
+        /// <summary>
+        /// 
+        /// </summary>
+        public double max
+        {
+            get
+            {
+                return _max;
+            }
+            set
+            {
+                if (_max != value)
+                {
+                    _max = value;
+                    Update(_value);
+                }
+            }
+        }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		public double max
-		{
-			get
-			{
-				return _max;
-			}
-			set
-			{
-				if (_max != value)
-				{
-					_max = value;
-					Update(_value);
-				}
-			}
-		}
+        /// <summary>
+        /// 
+        /// </summary>
+        public double value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                if (_tweening)
+                {
+                    GTween.Kill(this, TweenPropType.Progress, true);
+                    _tweening = false;
+                }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		public double value
-		{
-			get
-			{
-				return _value;
-			}
-			set
-			{
-				if (_tweening)
-				{
-					GTween.Kill(this, TweenPropType.Progress, true);
-					_tweening = false;
-				}
+                if (_value != value)
+                {
+                    _value = value;
+                    Update(_value);
+                }
+            }
+        }
 
-				if (_value != value)
-				{
-					_value = value;
-					Update(_value);
-				}
-			}
-		}
+        public bool reverse
+        {
+            get
+            {
+                return _reverse;
+            }
+            set
+            {
+                _reverse = value;
+            }
+        }
 
-		public bool reverse
-		{
-			get { return _reverse; }
-			set { _reverse = value; }
-		}
+        /// <summary>
+        /// 动态改变进度值。
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="duration"></param>
+        public GTweener TweenValue(double value, float duration)
+        {
+            double oldValule;
+            if (_tweening)
+            {
+                GTweener twener = GTween.GetTween(this, TweenPropType.Progress);
+                oldValule = twener.value.d;
 
-		/// <summary>
-		/// 动态改变进度值。
-		/// </summary>
-		/// <param name="value"></param>
-		/// <param name="duration"></param>
-		public GTweener TweenValue(double value, float duration)
-		{
-			double oldValule;
-			if (_tweening)
-			{
-				GTweener twener = GTween.GetTween(this, TweenPropType.Progress);
-				oldValule = twener.value.d;
-				twener.Kill(false);
-			}
-			else
-				oldValule = _value;
+                twener.Kill(false);
+            }
+            else
+                oldValule = _value;
 
-			_value = value;
-			_tweening = true;
-			return GTween.ToDouble(oldValule, _value, duration)
-				.SetEase(EaseType.Linear)
-				.SetTarget(this, TweenPropType.Progress)
-				.OnComplete(() => { _tweening = false; });
-		}
+            oldValule = _value;
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="newValue"></param>
-		public void Update(double newValue)
-		{
-			float percent = _max != 0 ? (float)Math.Min(newValue / _max, 1) : 0;
-			if (_titleObject != null)
-			{
-				switch (_titleType)
-				{
-					case ProgressTitleType.Percent:
+            _value = value;
+            _tweening = true;
+            Debug.Log($"旧进度Value值为{oldValule}");
+            Debug.Log($"新进度Value值为{_value}");
+            return GTween.ToDouble(oldValule, _value, duration)
+                    .SetEase(EaseType.Linear)
+                    .SetTarget(this, TweenPropType.Progress)
+                    .OnComplete(() => { _tweening = false; });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="newValue"></param>
+        public void Update(double newValue)
+        {
+            float percent = _max != 0? (float) Math.Min(newValue / _max, 1) : 0;
+            if (_titleObject != null)
+            {
+                switch (_titleType)
+                {
+                    case ProgressTitleType.Percent:
 #if RTL_TEXT_SUPPORT
 						if (RTLSupport.BaseDirection == RTLSupport.DirectionType.RTL)
 						{
@@ -147,11 +157,11 @@ namespace FairyGUI
 						else
 							_titleObject.text = Mathf.FloorToInt(percent * 100) + "%";
 #else
-						_titleObject.text = Mathf.FloorToInt(percent * 100) + "%";
+                        _titleObject.text = Mathf.FloorToInt(percent * 100) + "%";
 #endif
-						break;
+                        break;
 
-					case ProgressTitleType.ValueAndMax:
+                    case ProgressTitleType.ValueAndMax:
 #if RTL_TEXT_SUPPORT
 						if (RTLSupport.BaseDirection == RTLSupport.DirectionType.RTL)
 						{
@@ -160,142 +170,146 @@ namespace FairyGUI
 						else
 							_titleObject.text = Math.Round(newValue) + "/" + Math.Round(max);
 #else
-						_titleObject.text = Math.Round(newValue) + "/" + Math.Round(max);
+                        _titleObject.text = Math.Round(newValue) + "/" + Math.Round(max);
 #endif
-						break;
+                        break;
 
-					case ProgressTitleType.Value:
-						_titleObject.text = "" + Math.Round(newValue);
-						break;
+                    case ProgressTitleType.Value:
+                        _titleObject.text = "" + Math.Round(newValue);
+                        break;
 
-					case ProgressTitleType.Max:
-						_titleObject.text = "" + Math.Round(_max);
-						break;
-				}
-			}
+                    case ProgressTitleType.Max:
+                        _titleObject.text = "" + Math.Round(_max);
+                        break;
+                }
+            }
 
-			float fullWidth = this.width - _barMaxWidthDelta;
-			float fullHeight = this.height - _barMaxHeightDelta;
-			if (!_reverse)
-			{
-				if (_barObjectH != null)
-				{
-					if ((_barObjectH is GImage) && ((GImage)_barObjectH).fillMethod != FillMethod.None)
-						((GImage)_barObjectH).fillAmount = percent;
-					else if ((_barObjectH is GLoader) && ((GLoader)_barObjectH).fillMethod != FillMethod.None)
-						((GLoader)_barObjectH).fillAmount = percent;
-					else
-						_barObjectH.width = Mathf.RoundToInt(fullWidth * percent);
-				}
-				if (_barObjectV != null)
-				{
-					if ((_barObjectV is GImage) && ((GImage)_barObjectV).fillMethod != FillMethod.None)
-						((GImage)_barObjectV).fillAmount = percent;
-					else if ((_barObjectV is GLoader) && ((GLoader)_barObjectV).fillMethod != FillMethod.None)
-						((GLoader)_barObjectV).fillAmount = percent;
-					else
-						_barObjectV.height = Mathf.RoundToInt(fullHeight * percent);
-				}
-			}
-			else
-			{
-				if (_barObjectH != null)
-				{
-					if ((_barObjectH is GImage) && ((GImage)_barObjectH).fillMethod != FillMethod.None)
-						((GImage)_barObjectH).fillAmount = 1 - percent;
-					else if ((_barObjectH is GLoader) && ((GLoader)_barObjectH).fillMethod != FillMethod.None)
-						((GLoader)_barObjectH).fillAmount = 1 - percent;
-					else
-					{
-						_barObjectH.width = Mathf.RoundToInt(fullWidth * percent);
-						_barObjectH.x = _barStartX + (fullWidth - _barObjectH.width);
-					}
-				}
-				if (_barObjectV != null)
-				{
-					if ((_barObjectV is GImage) && ((GImage)_barObjectV).fillMethod != FillMethod.None)
-						((GImage)_barObjectV).fillAmount = 1 - percent;
-					else if ((_barObjectV is GLoader) && ((GLoader)_barObjectV).fillMethod != FillMethod.None)
-						((GLoader)_barObjectV).fillAmount = 1 - percent;
-					else
-					{
-						_barObjectV.height = Mathf.RoundToInt(fullHeight * percent);
-						_barObjectV.y = _barStartY + (fullHeight - _barObjectV.height);
-					}
-				}
-			}
-			if (_aniObject != null)
-				_aniObject.frame = Mathf.RoundToInt(percent * 100);
+            float fullWidth = this.width - _barMaxWidthDelta;
+            float fullHeight = this.height - _barMaxHeightDelta;
+            if (!_reverse)
+            {
+                if (_barObjectH != null)
+                {
+                    if ((_barObjectH is GImage) && ((GImage) _barObjectH).fillMethod != FillMethod.None)
+                        ((GImage) _barObjectH).fillAmount = percent;
+                    else if ((_barObjectH is GLoader) && ((GLoader) _barObjectH).fillMethod != FillMethod.None)
+                        ((GLoader) _barObjectH).fillAmount = percent;
+                    else
+                        _barObjectH.width = Mathf.RoundToInt(fullWidth * percent);
+                }
 
-			InvalidateBatchingState(true);
-		}
+                if (_barObjectV != null)
+                {
+                    if ((_barObjectV is GImage) && ((GImage) _barObjectV).fillMethod != FillMethod.None)
+                        ((GImage) _barObjectV).fillAmount = percent;
+                    else if ((_barObjectV is GLoader) && ((GLoader) _barObjectV).fillMethod != FillMethod.None)
+                        ((GLoader) _barObjectV).fillAmount = percent;
+                    else
+                        _barObjectV.height = Mathf.RoundToInt(fullHeight * percent);
+                }
+            }
+            else
+            {
+                if (_barObjectH != null)
+                {
+                    if ((_barObjectH is GImage) && ((GImage) _barObjectH).fillMethod != FillMethod.None)
+                        ((GImage) _barObjectH).fillAmount = 1 - percent;
+                    else if ((_barObjectH is GLoader) && ((GLoader) _barObjectH).fillMethod != FillMethod.None)
+                        ((GLoader) _barObjectH).fillAmount = 1 - percent;
+                    else
+                    {
+                        _barObjectH.width = Mathf.RoundToInt(fullWidth * percent);
+                        _barObjectH.x = _barStartX + (fullWidth - _barObjectH.width);
+                    }
+                }
 
-		override protected void ConstructExtension(ByteBuffer buffer)
-		{
-			buffer.Seek(0, 6);
+                if (_barObjectV != null)
+                {
+                    if ((_barObjectV is GImage) && ((GImage) _barObjectV).fillMethod != FillMethod.None)
+                        ((GImage) _barObjectV).fillAmount = 1 - percent;
+                    else if ((_barObjectV is GLoader) && ((GLoader) _barObjectV).fillMethod != FillMethod.None)
+                        ((GLoader) _barObjectV).fillAmount = 1 - percent;
+                    else
+                    {
+                        _barObjectV.height = Mathf.RoundToInt(fullHeight * percent);
+                        _barObjectV.y = _barStartY + (fullHeight - _barObjectV.height);
+                    }
+                }
+            }
 
-			_titleType = (ProgressTitleType)buffer.ReadByte();
-			_reverse = buffer.ReadBool();
+            if (_aniObject != null)
+                _aniObject.frame = Mathf.RoundToInt(percent * 100);
 
-			_titleObject = GetChild("title") as GTextField;
-			_barObjectH = GetChild("bar");
-			_barObjectV = GetChild("bar_v");
-			_aniObject = GetChild("ani") as GMovieClip;
+            InvalidateBatchingState(true);
+        }
 
-			if (_barObjectH != null)
-			{
-				_barMaxWidth = _barObjectH.width;
-				_barMaxWidthDelta = this.width - _barMaxWidth;
-				_barStartX = _barObjectH.x;
-			}
-			if (_barObjectV != null)
-			{
-				_barMaxHeight = _barObjectV.height;
-				_barMaxHeightDelta = this.height - _barMaxHeight;
-				_barStartY = _barObjectV.y;
-			}
-		}
+        override protected void ConstructExtension(ByteBuffer buffer)
+        {
+            buffer.Seek(0, 6);
 
-		override public void Setup_AfterAdd(ByteBuffer buffer, int beginPos)
-		{
-			base.Setup_AfterAdd(buffer, beginPos);
+            _titleType = (ProgressTitleType) buffer.ReadByte();
+            _reverse = buffer.ReadBool();
 
-			if (!buffer.Seek(beginPos, 6))
-			{
-				Update(_value);
-				return;
-			}
+            _titleObject = GetChild("title") as GTextField;
+            _barObjectH = GetChild("bar");
+            _barObjectV = GetChild("bar_v");
+            _aniObject = GetChild("ani") as GMovieClip;
 
-			if ((ObjectType)buffer.ReadByte() != packageItem.objectType)
-			{
-				Update(_value);
-				return;
-			}
+            if (_barObjectH != null)
+            {
+                _barMaxWidth = _barObjectH.width;
+                _barMaxWidthDelta = this.width - _barMaxWidth;
+                _barStartX = _barObjectH.x;
+            }
 
-			_value = buffer.ReadInt();
-			_max = buffer.ReadInt();
+            if (_barObjectV != null)
+            {
+                _barMaxHeight = _barObjectV.height;
+                _barMaxHeightDelta = this.height - _barMaxHeight;
+                _barStartY = _barObjectV.y;
+            }
+        }
 
-			Update(_value);
-		}
+        override public void Setup_AfterAdd(ByteBuffer buffer, int beginPos)
+        {
+            base.Setup_AfterAdd(buffer, beginPos);
 
-		override protected void HandleSizeChanged()
-		{
-			base.HandleSizeChanged();
+            if (!buffer.Seek(beginPos, 6))
+            {
+                Update(_value);
+                return;
+            }
 
-			if (_barObjectH != null)
-				_barMaxWidth = this.width - _barMaxWidthDelta;
-			if (_barObjectV != null)
-				_barMaxHeight = this.height - _barMaxHeightDelta;
+            if ((ObjectType) buffer.ReadByte() != packageItem.objectType)
+            {
+                Update(_value);
+                return;
+            }
 
-			if (!this.underConstruct)
-				Update(_value);
-		}
+            _value = buffer.ReadInt();
+            _max = buffer.ReadInt();
 
-		public override void Dispose()
-		{
-			if (_tweening)
-				GTween.Kill(this);
-			base.Dispose();
-		}
-	}
+            Update(_value);
+        }
+
+        override protected void HandleSizeChanged()
+        {
+            base.HandleSizeChanged();
+
+            if (_barObjectH != null)
+                _barMaxWidth = this.width - _barMaxWidthDelta;
+            if (_barObjectV != null)
+                _barMaxHeight = this.height - _barMaxHeightDelta;
+
+            if (!this.underConstruct)
+                Update(_value);
+        }
+
+        public override void Dispose()
+        {
+            if (_tweening)
+                GTween.Kill(this);
+            base.Dispose();
+        }
+    }
 }
