75df0243854e3703b6b54f37925840a5d3bd42be
NKGMadlife <1778139321@qq.com>
2020-07-29 23:56:24 +0800

    chore：清理旧版FGUI代码生成插件数据

--- Unity/Assets/Hotfix/NKGMOBA/FairyGUI/AutoGeneratedCode/FUIDialog/one_confirm.cs
+++ Unity/Assets/Hotfix/NKGMOBA/FairyGUI/AutoGeneratedCode/FUIDialog/one_confirm.cs
@@ -6,117 +6,119 @@ using ETModel;
 namespace ETHotfix
 {
     [ObjectSystem]
-    public class one_confirmAwakeSystem: AwakeSystem<one_confirm, GObject>
+    public class one_confirmAwakeSystem : AwakeSystem<one_confirm, GObject>
     {
         public override void Awake(one_confirm self, GObject go)
         {
             self.Awake(go);
         }
     }
-
-    public sealed class one_confirm: FUI
-    {
+        
+    public sealed class one_confirm : FUI
+    {	
         public const string UIPackageName = "FUIDialog";
         public const string UIResName = "one_confirm";
-
+        
         /// <summary>
         /// {uiResName}的组件类型(GComponent、GButton、GProcessBar等)，它们都是GObject的子类。
         /// </summary>
         public GButton self;
+            
+    public Controller button;
+    public GImage n0;
+    public GImage n1;
+    public GTextField title;
+    public const string URL = "ui://3gqem46sifyf2";
 
-        public Controller button;
-        public GImage n0;
-        public GImage n1;
-        public GTextField title;
-        public const string URL = "ui://3gqem46sifyf2";
-
-        private static GObject CreateGObject()
+    private static GObject CreateGObject()
+    {
+        return UIPackage.CreateObject(UIPackageName, UIResName);
+    }
+    
+    private static void CreateGObjectAsync(UIPackage.CreateObjectCallback result)
+    {
+        UIPackage.CreateObjectAsync(UIPackageName, UIResName, result);
+    }
+        
+    public static one_confirm CreateInstance()
+    {			
+        return ComponentFactory.Create<one_confirm, GObject>(CreateGObject());
+    }
+        
+    public static ETTask<one_confirm> CreateInstanceAsync(Entity domain)
+    {
+        ETTaskCompletionSource<one_confirm> tcs = new ETTaskCompletionSource<one_confirm>();
+        CreateGObjectAsync((go) =>
         {
-            return UIPackage.CreateObject(UIPackageName, UIResName);
+            tcs.SetResult(ComponentFactory.Create<one_confirm, GObject>(go));
+        });
+        return tcs.Task;
+    }
+        
+    public static one_confirm Create(GObject go)
+    {
+        return ComponentFactory.Create<one_confirm, GObject>(go);
+    }
+        
+    /// <summary>
+    /// 通过此方法获取的FUI，在Dispose时不会释放GObject，需要自行管理（一般在配合FGUI的Pool机制时使用）。
+    /// </summary>
+    public static one_confirm GetFormPool(GObject go)
+    {
+        var fui = go.Get<one_confirm>();
+        if(fui == null)
+        {
+            fui = Create(go);
         }
-
-        private static void CreateGObjectAsync(UIPackage.CreateObjectCallback result)
+        fui.isFromFGUIPool = true;
+        return fui;
+    }
+        
+    public void Awake(GObject go)
+    {
+        if(go == null)
         {
-            UIPackage.CreateObjectAsync(UIPackageName, UIResName, result);
+            return;
         }
-
-        public static one_confirm CreateInstance()
+        
+        GObject = go;	
+        
+        if (string.IsNullOrWhiteSpace(Name))
         {
-            return ComponentFactory.Create<one_confirm, GObject>(CreateGObject());
+            Name = Id.ToString();
         }
-
-        public static ETTask<one_confirm> CreateInstanceAsync(Entity domain)
-        {
-            ETTaskCompletionSource<one_confirm> tcs = new ETTaskCompletionSource<one_confirm>();
-            CreateGObjectAsync((go) => { tcs.SetResult(ComponentFactory.Create<one_confirm, GObject>(go)); });
-            return tcs.Task;
-        }
-
-        public static one_confirm Create(GObject go)
-        {
-            return ComponentFactory.Create<one_confirm, GObject>(go);
-        }
-
-        /// <summary>
-        /// 通过此方法获取的FUI，在Dispose时不会释放GObject，需要自行管理（一般在配合FGUI的Pool机制时使用）。
-        /// </summary>
-        public static one_confirm GetFormPool(GObject go)
-        {
-            var fui = go.Get<one_confirm>();
-            if (fui == null)
-            {
-                fui = Create(go);
-            }
-
-            fui.isFromFGUIPool = true;
-            return fui;
-        }
-
-        public void Awake(GObject go)
-        {
-            if (go == null)
+        
+        self = (GButton)go;
+        
+        self.Add(this);
+        
+        var com = go.asCom;
+            
+        if(com != null)
+        {	
+            
+    		button = com.GetControllerAt(0);
+    		n0 = (GImage)com.GetChildAt(0);
+    		n1 = (GImage)com.GetChildAt(1);
+    		title = (GTextField)com.GetChildAt(2);
+    	}
+}
+       public override void Dispose()
+       {
+            if(IsDisposed)
             {
                 return;
             }
-
-            GObject = go;
-
-            if (string.IsNullOrWhiteSpace(Name))
-            {
-                Name = Id.ToString();
-            }
-
-            self = (GButton) go;
-
-            self.Add(this);
-
-            var com = go.asCom;
-
-            if (com != null)
-            {
-                button = com.GetControllerAt(0);
-                n0 = (GImage) com.GetChildAt(0);
-                n1 = (GImage) com.GetChildAt(1);
-                title = (GTextField) com.GetChildAt(2);
-            }
-        }
-
-        public override void Dispose()
-        {
-            if (IsDisposed)
-            {
-                return;
-            }
-
+            
             base.Dispose();
-
+            
             self.Remove();
             self = null;
-
-            button = null;
-            n0 = null;
-            n1 = null;
-            title = null;
-        }
-    }
+            
+			button = null;
+			n0 = null;
+			n1 = null;
+			title = null;
+		}
+}
 }
--- Unity/Assets/Hotfix/NKGMOBA/FairyGUI/AutoGeneratedCode/FUIDialog/tow_cancel.cs
+++ Unity/Assets/Hotfix/NKGMOBA/FairyGUI/AutoGeneratedCode/FUIDialog/tow_cancel.cs
@@ -6,117 +6,119 @@ using ETModel;
 namespace ETHotfix
 {
     [ObjectSystem]
-    public class tow_cancelAwakeSystem: AwakeSystem<tow_cancel, GObject>
+    public class tow_cancelAwakeSystem : AwakeSystem<tow_cancel, GObject>
     {
         public override void Awake(tow_cancel self, GObject go)
         {
             self.Awake(go);
         }
     }
-
-    public sealed class tow_cancel: FUI
-    {
+        
+    public sealed class tow_cancel : FUI
+    {	
         public const string UIPackageName = "FUIDialog";
         public const string UIResName = "tow_cancel";
-
+        
         /// <summary>
         /// {uiResName}的组件类型(GComponent、GButton、GProcessBar等)，它们都是GObject的子类。
         /// </summary>
         public GButton self;
+            
+    public Controller button;
+    public GImage n0;
+    public GImage n1;
+    public GTextField title;
+    public const string URL = "ui://3gqem46sifyf5";
 
-        public Controller button;
-        public GImage n0;
-        public GImage n1;
-        public GTextField title;
-        public const string URL = "ui://3gqem46sifyf5";
-
-        private static GObject CreateGObject()
+    private static GObject CreateGObject()
+    {
+        return UIPackage.CreateObject(UIPackageName, UIResName);
+    }
+    
+    private static void CreateGObjectAsync(UIPackage.CreateObjectCallback result)
+    {
+        UIPackage.CreateObjectAsync(UIPackageName, UIResName, result);
+    }
+        
+    public static tow_cancel CreateInstance()
+    {			
+        return ComponentFactory.Create<tow_cancel, GObject>(CreateGObject());
+    }
+        
+    public static ETTask<tow_cancel> CreateInstanceAsync(Entity domain)
+    {
+        ETTaskCompletionSource<tow_cancel> tcs = new ETTaskCompletionSource<tow_cancel>();
+        CreateGObjectAsync((go) =>
         {
-            return UIPackage.CreateObject(UIPackageName, UIResName);
+            tcs.SetResult(ComponentFactory.Create<tow_cancel, GObject>(go));
+        });
+        return tcs.Task;
+    }
+        
+    public static tow_cancel Create(GObject go)
+    {
+        return ComponentFactory.Create<tow_cancel, GObject>(go);
+    }
+        
+    /// <summary>
+    /// 通过此方法获取的FUI，在Dispose时不会释放GObject，需要自行管理（一般在配合FGUI的Pool机制时使用）。
+    /// </summary>
+    public static tow_cancel GetFormPool(GObject go)
+    {
+        var fui = go.Get<tow_cancel>();
+        if(fui == null)
+        {
+            fui = Create(go);
         }
-
-        private static void CreateGObjectAsync(UIPackage.CreateObjectCallback result)
+        fui.isFromFGUIPool = true;
+        return fui;
+    }
+        
+    public void Awake(GObject go)
+    {
+        if(go == null)
         {
-            UIPackage.CreateObjectAsync(UIPackageName, UIResName, result);
+            return;
         }
-
-        public static tow_cancel CreateInstance()
+        
+        GObject = go;	
+        
+        if (string.IsNullOrWhiteSpace(Name))
         {
-            return ComponentFactory.Create<tow_cancel, GObject>(CreateGObject());
+            Name = Id.ToString();
         }
-
-        public static ETTask<tow_cancel> CreateInstanceAsync(Entity domain)
-        {
-            ETTaskCompletionSource<tow_cancel> tcs = new ETTaskCompletionSource<tow_cancel>();
-            CreateGObjectAsync((go) => { tcs.SetResult(ComponentFactory.Create<tow_cancel, GObject>(go)); });
-            return tcs.Task;
-        }
-
-        public static tow_cancel Create(GObject go)
-        {
-            return ComponentFactory.Create<tow_cancel, GObject>(go);
-        }
-
-        /// <summary>
-        /// 通过此方法获取的FUI，在Dispose时不会释放GObject，需要自行管理（一般在配合FGUI的Pool机制时使用）。
-        /// </summary>
-        public static tow_cancel GetFormPool(GObject go)
-        {
-            var fui = go.Get<tow_cancel>();
-            if (fui == null)
-            {
-                fui = Create(go);
-            }
-
-            fui.isFromFGUIPool = true;
-            return fui;
-        }
-
-        public void Awake(GObject go)
-        {
-            if (go == null)
+        
+        self = (GButton)go;
+        
+        self.Add(this);
+        
+        var com = go.asCom;
+            
+        if(com != null)
+        {	
+            
+    		button = com.GetControllerAt(0);
+    		n0 = (GImage)com.GetChildAt(0);
+    		n1 = (GImage)com.GetChildAt(1);
+    		title = (GTextField)com.GetChildAt(2);
+    	}
+}
+       public override void Dispose()
+       {
+            if(IsDisposed)
             {
                 return;
             }
-
-            GObject = go;
-
-            if (string.IsNullOrWhiteSpace(Name))
-            {
-                Name = Id.ToString();
-            }
-
-            self = (GButton) go;
-
-            self.Add(this);
-
-            var com = go.asCom;
-
-            if (com != null)
-            {
-                button = com.GetControllerAt(0);
-                n0 = (GImage) com.GetChildAt(0);
-                n1 = (GImage) com.GetChildAt(1);
-                title = (GTextField) com.GetChildAt(2);
-            }
-        }
-
-        public override void Dispose()
-        {
-            if (IsDisposed)
-            {
-                return;
-            }
-
+            
             base.Dispose();
-
+            
             self.Remove();
             self = null;
-
-            button = null;
-            n0 = null;
-            n1 = null;
-            title = null;
-        }
-    }
+            
+			button = null;
+			n0 = null;
+			n1 = null;
+			title = null;
+		}
+}
 }
