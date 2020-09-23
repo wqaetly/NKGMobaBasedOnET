0bcc414fa08ac418bb8fbf144909ffd8d54e2210
NKGMadlife <1778139321@qq.com>
2020-09-23 21:21:21 +0800

    chore&&res：阶段汇总提交，提交诺手技能图

--- Unity/Assets/Model/NKGMOBA/Battle/SkillSystem/Others/SkillNodeDataBase.cs
+++ Unity/Assets/Model/NKGMOBA/Battle/SkillSystem/Others/SkillNodeDataBase.cs
@@ -11,7 +11,7 @@ namespace ETModel
     public class BuffNodeDataBase
     {
         [LabelText("节点Id")]
-        [BoxGroup,GUIColor(	1,140/255f,0)]
+        [BoxGroup, GUIColor(1, 140 / 255f, 0)]
         public VTD_Id NodeId;
     }
 }
