2065c9a971e8d9c554ab86cf76974f5c6d3439e1
NKGMadlife <1778139321@qq.com>
2020-09-27 13:03:40 +0800

    fix：<技能编辑器>修复Buff链接部分层数会被重置的Bug

--- Unity/Assets/Plugins/NodeEditor/Editor/CustomNodes/SkillSystemNodes/SkillData/BuffNodes/ListenBuffCallBackBuffNode.cs
+++ Unity/Assets/Plugins/NodeEditor/Editor/CustomNodes/SkillSystemNodes/SkillData/BuffNodes/ListenBuffCallBackBuffNode.cs
@@ -4,6 +4,7 @@
 // 此代码由工具自动生成，请勿更改
 //------------------------------------------------------------
 
+using System.Collections.Generic;
 using ETModel;
 using NodeEditorFramework;
 using NodeEditorFramework.Utilities;
@@ -39,6 +40,14 @@ namespace SkillDemo
                 listenBuffCallBackBuffData.ListenBuffEventNormal = new ListenBuffEvent_Normal();
             }
 
+            //备份Buff Id和对应层数键值对，防止被覆写
+            Dictionary<long, int> buffDataBack = new Dictionary<long, int>();
+
+            foreach (var vtdBuffInfo in listenBuffCallBackBuffData.ListenBuffEventNormal.BuffInfoWillBeAdded)
+            {
+                buffDataBack.Add(vtdBuffInfo.BuffId.Value, vtdBuffInfo.Layers);
+            }
+
             listenBuffCallBackBuffData.ListenBuffEventNormal.BuffInfoWillBeAdded.Clear();
 
             foreach (var connection in this.connectionPorts)
@@ -55,6 +64,14 @@ namespace SkillDemo
                             {
                                 BuffId = targetNode.Skill_GetNodeData().NodeId
                             });
+                        }
+                    }
+
+                    foreach (var vtdBuffInfo in listenBuffCallBackBuffData.ListenBuffEventNormal.BuffInfoWillBeAdded)
+                    {
+                        if (buffDataBack.TryGetValue(vtdBuffInfo.BuffId.Value, out var buffLayer))
+                        {
+                            vtdBuffInfo.Layers = buffLayer;
                         }
                     }
                     return;
