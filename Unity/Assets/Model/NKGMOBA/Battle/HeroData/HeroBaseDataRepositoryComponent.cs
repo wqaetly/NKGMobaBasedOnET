cdecd0f91cced55572ed5dcc22c79f861350da6b
NKGMadlife <1778139321@qq.com>
2020-10-15 23:43:09 +0800

    fix：<HeroData>修复HeroData相关功能在出包时报错的问题

--- Unity/Assets/Model/NKGMOBA/Battle/HeroData/HeroBaseDataRepositoryComponent.cs
+++ Unity/Assets/Model/NKGMOBA/Battle/HeroData/HeroBaseDataRepositoryComponent.cs
@@ -8,6 +8,10 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using MongoDB.Bson.Serialization;
+#if !SERVER
+using UnityEngine;    
+#endif
+
 
 namespace ETModel
 {
@@ -26,18 +30,22 @@ namespace ETModel
 
         public void Awake()
         {
+#if SERVER
             byte[] mfile = File.ReadAllBytes("../Config/HeroBaseDatas/AllHeroDatas.bytes");
             this.AllHeroBaseDataDic = BsonSerializer.Deserialize<HeroDataSupportor>(mfile);
-            //Log.Info($"所读取的英雄属性大小为:{mfile.Length}");
-            /*Log.Info("开始进行Odin序列化反序列化测试");
-            actionTest = new TestAction();
-            this.actionTest.myAction += () => Console.WriteLine("666");
-            byte[] file = SerializationUtility.SerializeValue(actionTest, DataFormat.Binary);
-            Log.Info("序列化成功");
-            File.WriteAllBytes("../Config/HeroBaseDatas/testAction.bytes", file);
-            TestAction newtest = OdinSerializeHelper.DeSerialize<TestAction>("../Config/HeroBaseDatas/testAction.bytes");
-            Log.Info("反序列化成功");
-            newtest.myAction.Invoke();*/
+#else
+
+            ResourcesComponent resourcesComponent = Game.Scene.GetComponent<ResourcesComponent>();
+            GameObject heroDataConfigs = resourcesComponent.LoadAsset<GameObject>(ABPathUtilities.GetNormalConfigPath("HeroDataConfigs"));
+            foreach (var referenceCollectorData in heroDataConfigs.GetComponent<ReferenceCollector>().data)
+            {
+                TextAsset textAsset = heroDataConfigs.GetTargetObjectFromRC<TextAsset>(referenceCollectorData.key);
+
+                if (textAsset.bytes.Length == 0) Log.Info("没有读取到文件");
+
+                this.AllHeroBaseDataDic = BsonSerializer.Deserialize<HeroDataSupportor>(textAsset.bytes);
+            }
+#endif
         }
 
         /// <summary>
@@ -51,10 +59,11 @@ namespace ETModel
                 //Log.Info("序列化深拷贝");
                 return this.AllHeroBaseDataDic.MHeroDataSupportorDic[id].DeepCopy();
             }
+
             //Log.Error($"查询英雄基础数据失败,id为{id}");
             return null;
         }
-        
+
         /// <summary>
         /// 根据id来获取指定英雄数据(直接获得，请确保不会对数据进行修改！不然会导致未知错误)
         /// </summary>
@@ -64,8 +73,8 @@ namespace ETModel
             if (this.AllHeroBaseDataDic.MHeroDataSupportorDic.ContainsKey(id))
             {
                 return this.AllHeroBaseDataDic.MHeroDataSupportorDic[id];
-                
             }
+
             return null;
         }
     }
