8d4feff5e15c0cd2e34f9c782d44c919d791a70f
NKGMadlife <1778139321@qq.com>
2020-12-14 21:26:19 +0800

    feat：<数值系统>客户端热更层可以订阅Model层Numer事件

---
+++ Unity/Assets/Hotfix/NKGMOBA/Numeric/NumericChangeEvent_NotifyWatcher.cs
@@ -0,0 +1,14 @@
+﻿using ETModel;
+
+namespace ETHotfix
+{
+	// 分发数值监听
+	[Event(ETModel.EventIdType.NumbericChange)]
+	public class NumericChangeEvent_NotifyWatcher: AEvent<long, NumericType, float>
+	{
+		public override void Run(long id, NumericType numericType, float value)
+		{
+			Game.Scene.GetComponent<NumericWatcherComponent>().Run(numericType, id, value);
+		}
+	}
+}
